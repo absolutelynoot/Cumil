feac690867dac3259d2b9f7f1a7947e8ab87b816
Faisal Samudra <isalsamudra@hotmail.com>
2023-03-26 22:27:37 +0800

    Added hero, navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
+
+
 <template>
   <div>
+    <Navbar />
+    <div class="container"><Hero/></div>
     <button type="button" class="btn btn-primary">Primary</button>
   </div>
 </template>
 
 <script setup>
+
+import Navbar from './components/Navbar.vue'
+import Hero from './components/Hero.vue'
+
 </script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="px-4 pt-5 my-5 text-center border-bottom">
+        <h1 class="display-4 fw-bold text-body-emphasis">Not your regular popcorn</h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
+                popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system,
+                extensive prebuilt components, and powerful JavaScript plugins.</p>
+            
+        </div>
+        <div class="overflow-hidden" style="max-height: 30vh;">
+            <div class="container px-5">
+                <img src="bootstrap-docs.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image"
+                    width="700" height="500" loading="lazy">
+            </div>
+        </div>
+    </div>
+</template>
+
