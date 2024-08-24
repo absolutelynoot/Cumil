8a6dbb0fcbddd97cd0033e7980882060a8b191fc
Faisal Ichsan Samudra <39894980+absolutelynoot@users.noreply.github.com>
2024-08-24 22:23:26 +0800

    moved assets to public

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,12 +5,12 @@
             <p class="lead mb-4">Your go-to plant based squ'd snack. You won't even know it's plant based.</p>
         </div>
         <div class="d-flex justify-content-center">
-            <a class="text-body-secondary mx-3" href="https://www.tiktok.com/@cumil.sg?_t=8b2qYA7gUk6"><font-awesome-icon icon="fa-brands fa-tiktok" class="display-3"/></a>
-            <a class="text-body-secondary mx-3" href="https://instagram.com/cumil.sg?igshid=YjNmNGQ3MDY="><font-awesome-icon icon="fa-brands fa-instagram" class="display-3" /></a>
+            <a class="text-body-secondary mx-3" href="https:/www.tiktok.com/@cumil.sg?_t=8b2qYA7gUk6"><font-awesome-icon icon="fa-brands fa-tiktok" class="display-3"/></a>
+            <a class="text-body-secondary mx-3" href="https:/instagram.com/cumil.sg?igshid=YjNmNGQ3MDY="><font-awesome-icon icon="fa-brands fa-instagram" class="display-3" /></a>
         </div>
-        <img src="../assets/cumil.png" class="img-fluid img1" alt="Example image"
+        <img src="/cumil.png" class="img-fluid img1" alt="Example image"
                     width="700" height="900" loading="lazy">
-        <img src="../assets/cumil.png" class="img-fluid img2" alt="Example image"
+        <img src="/cumil.png" class="img-fluid img2" alt="Example image"
         width="700" height="900" loading="lazy">
     </div>
 </template>
